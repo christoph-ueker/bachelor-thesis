195d657eba2bbd474c7c0cb375813db225b4a3fc
Christoph Ueker <christoph.ueker@t-online.de>
2021-11-12 11:55:11 +0100

    local work

---
+++ helper.py
@@ -0,0 +1,13 @@
+""" POSITIONAL HELPER FUNCTIONS """
+
+
+def guard_label_pos(ori, tar):
+    return [(ori.pos[0] + ori.pos[1]) / 2, (tar.pos[0] + tar.pos[1]) / 2]
+
+
+def asgn_label_pos(ori, tar):
+    return [(ori.pos[0] + ori.pos[1]) / 2 + 5, (tar.pos[0] + tar.pos[1]) / 2]
+
+
+def sync_label_pos(ori, tar):
+    return [(ori.pos[0] + ori.pos[1]) / 2 + 10, (tar.pos[0] + tar.pos[1]) / 2]
