bee17a27d7e327d83a9c4d5a3f0f075118693a24
Christoph Ueker <christoph.ueker@t-online.de>
2021-11-12 11:45:42 +0100

    test

--- main.py
+++ main.py
@@ -7,6 +7,7 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+# test
 
 
 # Press the green button in the gutter to run the script.
